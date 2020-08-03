b90d25e6e3b6d53e66fac6ef2f26a4560c486b63
arunsnd2017 <arunsinghsnd@gmail.com>
2020-08-03 07:20:51 +0530

    formmtting products, category and user schema

--- models/category.js
+++ models/category.js
@@ -1,16 +1,17 @@
-const { Mongoose } = require("mongoose")
+const { Mongoose } = require("mongoose");
 
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-const categorySchema = new mongoose.Schema({
-    name:{
-        type: String,
-        trim: true,
-        required: 32,
-        unique: true
-    }
-},
-{timestamps: true}
+const categorySchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: 32,
+      unique: true,
+    },
+  },
+  { timestamps: true }
 );
 
-module.exports = mongoose.model("Category", categorySchema)
+module.exports = mongoose.model("Category", categorySchema);
