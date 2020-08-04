9329e812e1746c4923ebdbe99d1dc277ea76ca0a
arunsnd2017 <arunsinghsnd@gmail.com>
2020-08-04 10:46:23 +0530

    adding common middlware and using there cors, bodypaser, cookie-parser

--- app.js
+++ app.js
@@ -1,17 +1,26 @@
-require('dotenv').config()
-const mongoose = require("mongoose");
+require('dotenv').config()      
+
+const mongoose = require('mongoose')
 const express = require('express')
 const app = express();
 
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
+const cors = require('cors')
+
 
 mongoose.connect(process.env.DATABASE, {
-     useNewUrlParser: true,
+    useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true
 })
 .then(() =>{
     console.log("DB CONNECTED")
 })
+
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(cors());
 
 
 const port = process.env.PORT || 8000;
