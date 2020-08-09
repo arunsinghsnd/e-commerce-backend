85e674d368a9f736ad0fdfc1ba7cb2a5a7058e11
arunsnd2017 <arunsinghsnd@gmail.com>
2020-08-09 19:59:09 +0530

    setting up validation in routes

--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,15 @@
-const { Router } = require("express");
 var express = require("express");
 const { models } = require("mongoose");
 var router = express.Router();
+const { check } = require('express-validator');
 const {signout, signup} = require("../controllers/auth");
 
 
-router.post("/signup", signup);
+router.post("/signup",[
+    check("name", "name should be at least 3 character").isLength({ min: 3}),
+    check("email", "email is required").isEmail(),
+    check("password","password should be at least 3 char").isLength({min: 3})
+], signup);
 router.get("/signout", signout);
 
 module.exports = router;
