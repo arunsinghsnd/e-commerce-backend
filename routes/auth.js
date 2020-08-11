23108e4628e9fff6ae863b4bd5751541941206df
arunsnd2017 <arunsinghsnd@gmail.com>
2020-08-11 12:10:36 +0530

    adding the custom middlewares for user and admin

--- routes/auth.js
+++ routes/auth.js
@@ -19,8 +19,6 @@ router.post("/signin",[
 
 router.get("/signout", signout);
 
-router.get("/testroute", isSignedIn, (req,res) =>{
-    res.send("A protected route");
-});
+
 
 module.exports = router;
