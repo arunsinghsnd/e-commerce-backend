7c6696ce2e79fd192bfa05ab7801e9dd64c1e7f2
arunsnd2017 <arunsinghsnd@gmail.com>
2020-08-11 11:37:55 +0530

    working on signout routes

--- controllers/auth.js
+++ controllers/auth.js
@@ -60,7 +60,8 @@ exports.signin = (req, res) => {
 };
 
 exports.signout = (req, res) => {
+    res.clearCookie("token");
   res.json({
-    message: "User Signout !!",
+    message: "User Signout Successfully",
   });
 };
