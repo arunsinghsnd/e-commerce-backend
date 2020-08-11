9fff334f6f2094edcf8350efc1e0de243a2344a3
arunsnd2017 <arunsinghsnd@gmail.com>
2020-08-11 11:15:28 +0530

    sending tokens in cookie for user

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,32 +1,65 @@
 const User = require("../models/user");
-const { validationResult } = require('express-validator');
+const { check, validationResult } = require("express-validator");
+var expressJwt = require('express-jwt');
+var jwt = require('jsonwebtoken');
+const { json } = require("body-parser");
 
-exports.signup = (req,res) =>{
-    const errors = validationResult(req)
+exports.signup = (req, res) => {
+  const errors = validationResult(req);
 
-    if(!errors.isEmpty()){
-        return res.status(422).json({
-            error: errors.array()[0].msg
+  if (!errors.isEmpty()) {
+    return res.status(422).json({
+      error: errors.array()[0].msg,
+    });
+  }
+
+  const user = new User(req.body);
+  user.save((err, user) => {
+    if (err) {
+      return res.status(400).json({
+        err: "NOT able to save user in DB",
+      });
+    }
+    res.json({
+      name: user.name,
+      email: user.email,
+      id: user._id,
+    });
+  });
+};
+
+exports.signin = (req, res) => {
+  const { email, password } = req.body;
+  if (!errors.isEmpty()) {
+    return res.status(422).json({
+      error: errors.array()[0].msg,
+    });
+  }
+  User.findOne({email}, (err , user) =>{
+    if(err){
+        res.status(400).json({
+            error: "User emial does not exists"
         })
     }
+    if(!user.autheticate(password)){
+    return   res.status(401).json({
+            error: "Email and Pasword Do  not match"
+        })
+    }
+    // Create token
+    const token = jwt.sign({_id: user._id}, process.env.SECRET)
+    //put token in cookie
+    res.cookie("token", token, {expire: new Date() + 9999});
 
-    const user = new User(req.body)
-    user.save((err, user) => {
-        if(err){
-            return res.status(400).json({
-                err: "NOT able to save user in DB"
-            })
-        }
-        res.json({
-            name: user.name,
-            email: user.email,
-            id: user._id
-        });
-    });
+    // send response to front end
+
+    const {_id, name , email , role} = user;
+    return res.json({token , user: {_id, name, email , role}});
+  });
 };
 
 exports.signout = (req, res) => {
-    res.json({
-        message: "User Signout !!"
-    });
+  res.json({
+    message: "User Signout !!",
+  });
 };
