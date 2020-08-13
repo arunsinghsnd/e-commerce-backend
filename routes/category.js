1eaa9925852b724a9fc4590e8879a0dcbd245f90
arunsnd2017 <arunsinghsnd@gmail.com>
2020-08-13 05:43:49 +0530

    Update the collections

--- routes/category.js
+++ routes/category.js
@@ -1,7 +1,7 @@
 const  express = require("express");
 const router =  express.Router();
 
-const {getCategoryById, createCategory, getCategory, getAllCategory} = require("../controllers/category")
+const {getCategoryById, createCategory, getCategory, getAllCategory, updateCategory} = require("../controllers/category")
 const {isSignedIn, isAdmin ,isAuthenticated} = require("../controllers/auth")
 const {getUserById} = require("../controllers/user")
 
@@ -12,9 +12,25 @@ router.param("categoryId", getCategoryById);
 
 //actual routes goes here
 
+
+// creates routes
 router.post("/category/create/:userId",isSignedIn ,isAuthenticated, isAdmin, createCategory);
 
+
+// read routes
 router.get("/category/:categoryId", getCategory)
 router.get("/categories", getAllCategory)
 
+
+// update
+router.put(
+    "/category/:categoryId/:userId",
+    isSignedIn ,
+    isAuthenticated, 
+    isAdmin, 
+    updateCategory
+);
+
+
+// delete
 module.exports = router;
