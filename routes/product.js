716cc4f0818174790c7afeda13fbefbf8eedcb36
arunsnd2017 <arunsinghsnd@gmail.com>
2020-08-14 07:34:28 +0530

    update and delete the T-shirt

--- routes/product.js
+++ routes/product.js
@@ -1,7 +1,14 @@
 const express = require("express")
 const router = express.Router();
 
-const  {getProductById , createProduct, getProduct, photo} = require("../controllers/product")
+const  {
+    getProductById , 
+    createProduct, 
+    getProduct, 
+    photo,
+    deleteProduct,
+    updateProduct
+} = require("../controllers/product")
 const  {isSignedIn, isAuthenticated, isAdmin} = require("../controllers/auth")
 const  { getUserById } = require("../controllers/user")
 
@@ -11,7 +18,16 @@ router.param("userId", getUserById);
 router.param("productId", getProductById);
 
 // all of actul routes
+//create route
 router.post("/product/create/:userId",isSignedIn, isAuthenticated, isAdmin, createProduct);
+
+//delete route
+router.delete("/product/:productId/:userId", isSignedIn,isAuthenticated,isAdmin, deleteProduct);
+
+//update route
+router.put("/product/:productId/:userId", isSignedIn,isAuthenticated,isAdmin, updateProduct);
+
+//listing route
 
 router.get("/product/:productId", getProduct);
 router.get("/product/photo/:productId", photo);
